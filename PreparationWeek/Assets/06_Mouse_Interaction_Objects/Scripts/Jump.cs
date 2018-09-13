f7e2d55ceab44575def2fd2c5564a52eab53c67a
Tibor Udvari <tibor.udvari@icloud.com>
2018-09-13 14:45:07 +0200

    Jumpy cube

---
+++ PreparationWeek/Assets/06_Mouse_Interaction_Objects/Scripts/Jump.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+    public float intensity = 10f;
+
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    public void JumpNow() {
+        rb.AddForce(Vector3.up * intensity, ForceMode.Impulse);
+    }
+}
