1501999b7d88e0a6a8fb857e54ad4a672746806d
Tibor Udvari <tibor.udvari@icloud.com>
2018-09-13 16:21:40 +0200

    Script communication

--- PreparationWeek/Assets/07_snake/script/BouffeGenerator.cs
+++ PreparationWeek/Assets/07_snake/script/BouffeGenerator.cs
@@ -5,14 +5,8 @@ using UnityEngine;
 public class BouffeGenerator : MonoBehaviour {
     public GameObject bouffe;
 	// Use this for initialization
-	void Start () {
+    public void Generate(){
         GameObject newBouffe = Instantiate(bouffe);
         newBouffe.transform.position = new Vector3(Random.Range(-10, 10), 0.5f, Random.Range(-10, 10));
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
--- PreparationWeek/Assets/07_snake/script/snake_eat.cs
+++ PreparationWeek/Assets/07_snake/script/snake_eat.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class snake_eat : MonoBehaviour {
-
+    public BouffeGenerator bouffegenerator;
     //Add IsTrigger on collider and RigidBody on player.
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "bouffe"){
             Debug.Log("miam");
             Destroy(other.gameObject);
+            bouffegenerator.Generate();
         }    
     }
 
