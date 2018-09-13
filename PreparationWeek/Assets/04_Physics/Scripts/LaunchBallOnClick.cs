4fdee7fa0febf9a8b7f13fa422a81ff07b1572e1
Tibor Udvari <tibor.udvari@icloud.com>
2018-09-13 12:00:05 +0200

    Physics engine interaction

---
+++ PreparationWeek/Assets/04_Physics/Scripts/LaunchBallOnClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchBallOnClick : MonoBehaviour {
+
+    Rigidbody rb;
+    public float intensity = 1f;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		
+        // When click
+        if (Input.GetMouseButtonDown(0)){
+            Debug.Log("Mouse clicked");
+            rb.AddForce(Vector3.forward * intensity, ForceMode.Acceleration);
+        }
+
+	}
+}
