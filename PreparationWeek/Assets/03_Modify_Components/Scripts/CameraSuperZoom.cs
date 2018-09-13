fe0b984a7029b37917d17369dcb92e4b90dea578
Tibor Udvari <tibor.udvari@icloud.com>
2018-09-13 11:38:52 +0200

    Modify components / camera superzoom

---
+++ PreparationWeek/Assets/03_Modify_Components/Scripts/CameraSuperZoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSuperZoom : MonoBehaviour {
+
+    Camera myCamera;
+
+	void Start () {
+        // 160 -> 20
+        // Get object
+        myCamera = gameObject.GetComponent<Camera>();
+	}
+	
+	void Update () {
+        // Change zoom
+        //myCamera.fieldOfView = 20f;
+
+        // Animate zoom
+        /*
+        if (myCamera.fieldOfView > 20f) { // Animate only if larger than 20f
+            myCamera.fieldOfView -= 30f * Time.deltaTime;
+        }*/
+
+        // Animate zoom and stop with max function
+        float newFieldOfView = myCamera.fieldOfView - 30f * Time.deltaTime;
+        float contrainedFieldOfView = Mathf.Max(newFieldOfView, 20f);
+        myCamera.fieldOfView = contrainedFieldOfView;
+
+    }
+}
