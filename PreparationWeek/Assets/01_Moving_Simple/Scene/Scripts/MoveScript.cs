b961089058bd55573ac37f3adf601065d13d8ca4
Tibor Udvari <tibor.udvari@icloud.com>
2018-09-13 10:07:19 +0200

    Moving an Object

---
+++ PreparationWeek/Assets/01_Moving_Simple/Scene/Scripts/MoveScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+	void Start () {
+        Debug.Log(gameObject.name);
+	}
+	
+    // Like draw in Processing
+    void Update () {
+        transform.position = transform.position + Vector3.left * 0.1f * Time.deltaTime;
+	}
+}
