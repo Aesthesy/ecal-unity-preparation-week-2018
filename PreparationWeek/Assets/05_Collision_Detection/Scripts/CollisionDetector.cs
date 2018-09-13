ed0e71bc93468cb390e3441a98a7d1f1273665ed
Tibor Udvari <tibor.udvari@icloud.com>
2018-09-13 14:31:34 +0200

    Baked lights

--- PreparationWeek/Assets/05_Collision_Detection/Scripts/CollisionDetector.cs
+++ PreparationWeek/Assets/05_Collision_Detection/Scripts/CollisionDetector.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Like a processing library
+using UnityEngine.SceneManagement;
+
 public class CollisionDetector : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision)
@@ -9,20 +12,18 @@ public class CollisionDetector : MonoBehaviour {
         // Check collision by name - not best way
         /* if (collision.collider.gameObject.name == "Cube") {
             Debug.Log("We hit the cube");
-        }*/ 
+        }*/
+
+        GetComponent<AudioSource>().Play();
 
         // Check collision by tag
         if (collision.collider.gameObject.tag == "target")
         {
             Debug.Log("We hit the cube");
+            // Winning sound
+            // Restart level
+            SceneManager.LoadScene("CollisionDetection");
         }
     }
 
-    void Start () {
-		
-	}
-	
-	void Update () {
-		
-	}
 }
